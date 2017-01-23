cd9fee9112552a8ac8ea84151d41cef7fa09f5d3
ch4p43v <ch4p43v@users.noreply.github.com>
2017-01-23 16:34:27 +0100

    Fix sort errors + speedup

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# aarray
-Associative array
+# Sortable associative array
+
+
+```javascript
+var mydata = new AArray({'c':'3', 'b':'2', 'a':'1'});
+for (var i in mydata) {
+	console.info(i+':'+mydata[i]);
+}
+// Output:
+// c:3
+// b:2
+// a:1
+
+mydata.sort();
+for (var i in mydata) {
+	console.info(i+':'+mydata[i]);
+}
+// Output:
+// a:1
+// b:2
+// c:3
+
+mydata.ksort();
+for (var i in mydata) {
+	console.info(i+':'+mydata[i]);
+}
+// Output:
+// a:1
+// b:2
+// c:3
+
+```
+# Properties
+* length (element count)
+
+# Functions
+* exend(Object)
+* remove(mixed key)
+* Number nextKey() get next aviable key
+* mixed push(mixed value[, mixed key]) key is optional, result is key
+* forEach(Function eachFunction(mixed value, mixed, key, aarry context))
+* Object shiftObject() Result ist {key:mixed, value:mixed}
+* Number length()
+* sort([Function f])
+* ksort([Function f])
+* reverse()
+* Array keys()
+* Array values()
+
+```javascript
+var AArray = require("aarray");
+var mydata = new AArray();
+
+// assign
+mydata['abc'] = 'xyz';
+mydata[1] = 'xyz';
+var newIndex = mydata.push('xyz');
+mydata.push('xyz', 'abc'); // replace first item
+
+// loop
+mydata.forEach(function(val, key, context) {
+    // do somthing
+    // remove item? No problem
+    mydata.remove(key) // or delete mydata[key];
+});
+```
--- aarray.js
+++ aarray.js
@@ -4,141 +4,194 @@
  * @constructor
  */
 function AArray(data) {
-    if (data) AArray.extend(data);
-};
+	this.length = 0;
+	this.keySet = [];
+	this.valueSet = [];
 
-AArray.prototype.exend = function(data) {
-    if (data instanceof Array) {
-        for(var i=0; i < data.length; i++) this[i] = data[i];
-    } 
-    else if (data instanceof Array) {
-        for (var i in data) this[i] = data[i];
-    }
-    
-    return this;
-};
+	this.extend = function(data) {
+		if (data instanceof Array) for(var i=0; i < data.length; i++) this[i] = data[i];
+		else if (data && data instanceof Object) for(i in data) this[i] = data[i];
+		return this;
+	};
 
-/**
- * Elemente durchlaufen
- * @param {Function({*}, {*})} eachFunction
- */
-AArray.prototype.each = function(eachFunction) {
-    var keys = this.keys(), key, res;
-    while(keys.length > 0) {
-        key = keys.shift();
-        res = eachFunction.call(this, key, this[key]);
-        if (typeof res == 'boolean' && !res) break;
-    }
-};
+	this.remove = function(key) { return delete this[key]; };
 
-/**
- * Ein item mit incl. Key holen
- * @return {{key: {*}, value: {*}}
- */
-AArray.prototype.shiftObject = function() {
-    var keys = this.keys(), res = null, key = null;
-    
-    if (keys && keys.length > 0) {
-        key = keys.shift();
-        if (typeof this[key] != 'undefined') {
-            res = {
-                key: key,
-                value: this[key]
-            };
-            delete this[key];
-        }
-    }
-    return res;
+	this.nextKey = function() {
+		var res = 0;
+		for(var i = 0; i < this.keySet.length; i++) {
+			if (!isNaN(this.keySet[i]) && this.keySet[i].indexOf('.') == -1 && res < parseInt(this.keySet[i])) res = parseInt(this.keySet[i]);
+		}
+		return res;
+	};
+
+	/**
+	 * Add Element to Array
+	 * @param {*} value
+	 * @param {*} [key] Optional
+	 */
+	this.push = function(value, key) {
+		if (typeof key == 'undefined' || key === null) key = this.nextKey();
+		this[key] = value;
+		return key;
+	};
+
+	/**
+	 * Elemente durchlaufen
+	 * @depricated
+	 * @param {Function({*}, {*})} eachFunction
+	 */
+	this.each = function(eachFunction) {
+		var res;
+		for(var i=0; i < this.keySet.length; i++) {
+			res = eachFunction.call(this, this.keySet[i], this.valueSet[i]);
+			if (typeof res == 'boolean' && !res) break;
+		}
+	};
+
+	/**
+	 * Elemente durchlaufen
+	 * @param {Function({*}, {*})} eachFunction
+	 */
+	this.forEach = function(eachFunction) {
+		var res;
+		for(var i=0; i < this.keySet.length; i++) {
+			res = eachFunction.call(this, this.valueSet[i], this.keySet[i]);
+			if (typeof res == 'boolean' && !res) break;
+		}
+	};
+
+	/**
+	 * Ein item mit incl. Key holen
+	 * @return {{key: {*}, value: {*}}
+	 */
+	this.shiftObject = function() {
+		if (this.keySet.length > 0) return { key: this.keySet.shift(), value: this.valueSet.shift() };
+		return null;
+	}
+
+	/**
+	 * Erstes element entfernen
+	 * @return {*}
+	 */
+	this.shift = function() {
+		if (this.keySet.length > 0) {
+			this.keySet.shift();
+			return this.valueSet.shift();
+		}
+		return  null;
+	};
+
+	/**
+	 * Anzal der Elemente ermitteln
+	 */
+	this.length = function() {
+		return this.length;
+	};
+
+	/**
+	 * Sortieren nach Value
+	 * @param {Function} [func] Vergleichsfunktion
+	 */
+	this.sort = function(sortFunc) {
+		var sortable = [], row;
+		if (this.keySet.length > 0) {
+			while(this.keySet.length > 0) {
+				sortable.push([this.keySet.shift(), this.valueSet.shift()]);
+			}
+
+			sortable.sort(function(a, b) {
+				if (sortFunc instanceof Function) return sortFunc(a[1], b[1]);
+				else return a[1] - b[1];
+			});
+
+			while(sortable.length > 0) {
+				row = sortable.shift();
+				this.keySet.push(row[0]);
+				this.valueSet.push(row[1]);
+			}
+		}
+	};
+
+	/**
+	 * Nach Schlüssel sortieren
+	 * @param {Function} [func] Vergleichsfunktion
+	 */
+	this.ksort = function(sortFunc) {
+		var sortable = [], row;
+		if (this.keySet.length > 0) {
+			while(this.keySet.length > 0) {
+				sortable.push([this.keySet.shift(), this.valueSet.shift()]);
+			}
+
+			sortable.sort(function(a, b) {
+				if (sortFunc instanceof Function) return sortFunc(a[0], b[0]);
+				else return a[0] - b[0];
+			});
+
+			while(sortable.length > 0) {
+				row = sortable.shift();
+				this.keySet.push(row[0]);
+				this.valueSet.push(row[1]);
+			}
+		}
+	};
+
+	/**
+	 * Sortierung umkehren
+	 * @param {Function} [func] Vergleichsfunktion
+	 */
+	this.reverse = function() {
+		this.keySet.reverse();
+		this.valueSet.reverse();
+	};
+
+	/**
+	 * Schlüssel als ein Array holen
+	 */
+	this.keys = function() { return this.keySet; };
+
+	/**
+	 * Werte als ein Array holen
+	 */
+	this.values = function() { return this.valueSet; };
+
+	if (data) this.extend(data);
 }
 
-/**
- * Erstes element entfernen
- * @return {*}
- */
-AArray.prototype.shift = function() {
-    var res = this.shiftObject();
-    return res ? res['value'] : null;
+module.exports = function(data) {
+	return new Proxy(new AArray(data), {
+		deleteProperty: function(target, name) {
+			delete target[name];
+			var i = target.keySet.indexOf(name);
+			if (i > -1) {
+				target.keySet.splice(i, 1);
+				target.valueSet.splice(i, 1);
+			}
+			target.length = target.keySet.length;
+			return true;
+		},
+		ownKeys: function (target) {
+			return target.keySet;
+		},
+		get: function(target, name, receiver) {
+			if (name == 'constructor') return target[name];
+			var i = target.keySet.indexOf(name);
+			if (i > -1) return target.valueSet[i];
+			else return target[name];
+		},
+		set: function(target, name, value, receiver) {
+			var i = target.keySet.indexOf(name);
+			if (i > -1) {
+				target.valueSet[i] = value;
+			}
+			else {
+				var i = target.keySet.push(name)-1;
+				target.valueSet[i] = value;
+			}
+			// define property
+			target[name] = null; // Platzhalter damit man mit Objekte wie gewohnt arbeiten kann ;-)
+			target.length = target.keySet.length;
+			return true;
+		}
+	});
 };
-
-/**
- * Anzal der Elemente ermitteln
- */
-AArray.prototype.length = function() {
-    return this.keys().length;
-};
-
-/**
- * Sortieren nach Value
- * @param {Function} [func] Vergleichsfunktion
- */
-AArray.prototype.sort = function(sortFunc) {
-    var sortable = [], keys = this.keys(), key, row;
-    if (keys.length > 0) {
-	    while(keys.length > 0) {
-		    key = keys.shift();
-		    sortable.push([key, this[key]]);
-		    delete this[key];
-	    }
-
-	    sortable.sort(function(a, b) {
-		    if (sortFunc instanceof Function) {
-			    return sortFunc(a[1], b[1]);
-		    }
-		    else return a[1] - b[1];
-	    });
-
-	    while(sortable.length > 0) {
-		    row = sortable.shift();
-		    this[row[0]] = row[1];
-	    }
-    }
-};
-
-/**
- * Nach Schlüssel sortieren
- * @param {Function} [func] Vergleichsfunktion
- */
-AArray.prototype.ksort = function(func) {
-    //if (!func || !(func instanceof Function)) func = ;
-    var keysSorted = this.keys().sort(func);
-    var key, value;
-    while(keysSorted.length > 0) {
-        key = keysSorted.shift();
-        value = this[key];
-        delete this[key];
-        this[key] = value;
-    }
-};
-
-/**
- * Sortierung umkehren
- * @param {Function} [func] Vergleichsfunktion
- */
-AArray.prototype.reverse = function() {
-    var keys = this.keys(), key, value;
-    while(keys.length > 0) {
-        key = keys.pop();
-        value = this[key];
-        delete this[key];
-        this[key] = value;
-    }
-};
-
-/**
- * Schlüssel als ein Array holen
- */
-AArray.prototype.keys = function() { return Object.keys(this); };
-
-/**
- * Schlüssel als ein Array holen
- */
-AArray.prototype.values = function() { 
-    var o = [], keys = this.keys();
-    while(keys.length > 0) {
-        var i = keys.shift();
-        o.push(this[i]);
-    }
-    return o;
-};
-
-module.exports = AArray;
